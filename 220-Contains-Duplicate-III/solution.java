eb3e1bfba29fe17887fe1426dd5783d17adb01c9
QIANG ZHANG <zqfly2013@gmail.com>
2016-04-08 15:44:09 -0400

    [220. Contains Duplicate III][Runtime Error]committed by johnson

---
+++ 220-Contains-Duplicate-III/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < map.size(); j++) {
+                if (Math.abs(map.get(j) - nums[i]) <= t && Math.abs(i - j) <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
