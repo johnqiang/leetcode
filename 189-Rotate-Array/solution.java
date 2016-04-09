0132dab210e6d0b4024de3418be95f7d58c0b840
QIANG ZHANG <zqfly2013@gmail.com>
2016-04-09 17:34:54 -0400

    [189. Rotate Array][Accepted]committed by johnson

--- 189-Rotate-Array/solution.java
+++ 189-Rotate-Array/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public void rotate(int[] nums, int k) {
         if (nums.length < 2)
             return;
-        int[] temp = nums;
+        int[] temp = Arrays.copyOf(nums, nums.length);
         for(int i = 0; i < nums.length; i++) {
             nums[(i+k)%nums.length] = temp[i];
         }
