d1d8b13050ddca8759bb7bc1dea7f5e449616663
QIANG ZHANG <zqfly2013@gmail.com>
2016-04-09 18:48:33 -0400

    [189. Rotate Array][Wrong Answer]committed by johnson

--- 189-Rotate-Array/solution.java
+++ 189-Rotate-Array/solution.java
@@ -4,9 +4,12 @@ public class Solution {
             return;
         // int[] temp = Arrays.copyOf(nums, nums.length); // copy array cost O(n) extra space
         int index = 0;
+        int temp0 = nums[0];
+        int temp1 = nums[0];
         for(int i = 0; i < nums.length; i++) {
-            int temp = nums[(index+k)%nums.length]; // now extra space O(1)
-            nums[(index+k)%nums.length] = nums[index];
+            temp0 = temp1;
+            temp1 = nums[(index+k)%nums.length]; // now extra space O(1)
+            nums[(index+k)%nums.length] = temp0;
             index = (index+k)%nums.length;
         }
     }
