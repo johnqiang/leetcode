f633189e30ed661a552befdf89b01c2f3bc67f9d
QIANG ZHANG <zqfly2013@gmail.com>
2016-04-09 17:11:03 -0400

    [189. Rotate Array][Runtime Error]committed by johnson

--- 189-Rotate-Array/solution.java
+++ 189-Rotate-Array/solution.java
@@ -1,11 +1,11 @@
 public class Solution {
     public void rotate(int[] nums, int k) {
+        int[] temp = nums;
         for(int i = 0; i < k; i++) {
-            int temp = nums[nums.length-1];
-            for(int j = nums.length-1; j > 0; j--) {
-                nums[j] = nums[j-1];
-            }
-            nums[0] = temp;
+            nums[i] = temp[nums.length-k+i];
+        }
+        for(int j = k; j < nums.length; j++) {
+            nums[j] = temp[j-k];
         }
     }
 }
