a53a4c8348c64babaddc36f192c0805dd349f300
QIANG ZHANG <zqfly2013@gmail.com>
2016-04-10 01:05:41 -0400

    [66. Plus One][Accepted]committed by johnson

--- 66-Plus-One/solution.java
+++ 66-Plus-One/solution.java
@@ -4,6 +4,14 @@ public class Solution {
         int sum = digits[i]+1;
         while(sum == 10) {
             digits[i] = 0;
+            if(i == 0) {
+                int[] temp = new int[digits.length+1];
+                for(int j = 0; j < digits.length; j++) {
+                    temp[j+1] = digits[j];
+                }
+                temp[0] = 1;
+                return temp;
+            }
             i--;
             sum = digits[i]+1;
         }
