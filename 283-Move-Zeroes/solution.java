3f076437257960d64e131fd535263fb472c84cf1
QIANG ZHANG <zqfly2013@gmail.com>
2016-04-10 23:47:43 -0400

    [283. Move Zeroes][Accepted]committed by johnson

--- 283-Move-Zeroes/solution.java
+++ 283-Move-Zeroes/solution.java
@@ -1,13 +1,21 @@
 public class Solution {
     public void moveZeroes(int[] nums) {
-        for(int j = 0; j < nums.length-1; j++){
-            for(int i = 0; i < nums.length-1-j; i++) {
-                if (nums[i] == 0) {
-                    int temp = nums[i];
-                    nums[i] = nums[i+1];
-                    nums[i+1] = temp;
-                }
-            }
+        // for(int j = 0; j < nums.length-1; j++){ // stupid way !
+        //     for(int i = 0; i < nums.length-1-j; i++) {
+        //         if (nums[i] == 0) {
+        //             int temp = nums[i];
+        //             nums[i] = nums[i+1];
+        //             nums[i+1] = temp;
+        //         }
+        //     }
+        // }
+        // clever way !
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) 
+                nums[count++] = nums[i];
         }
+        while(count < nums.length)
+            nums[count++] = 0;
     }
 }
