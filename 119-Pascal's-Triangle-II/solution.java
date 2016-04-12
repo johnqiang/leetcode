c0e1217cc841c667197aa4c8b756cc466f326e8c
QIANG ZHANG <zqfly2013@gmail.com>
2016-04-12 12:37:21 -0400

    [119. Pascal's Triangle II][Accepted]committed by johnson

--- 119-Pascal's-Triangle-II/solution.java
+++ 119-Pascal's-Triangle-II/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> res = new ArrayList<Integer>();
         res.add(1);
-        for(int i = 1; i < rowIndex; i++) {
+        for(int i = 1; i <= rowIndex; i++) {
             for(int j = i-1; j > 0; j--) {
                 res.set(j, res.get(j-1)+res.get(j));
             }
