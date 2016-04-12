7084df80dedee469b4cbedd65bea21717ec37a79
QIANG ZHANG <zqfly2013@gmail.com>
2016-04-11 23:57:24 -0400

    [121. Best Time to Buy and Sell Stock][Accepted]committed by johnson

--- 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
@@ -4,7 +4,7 @@ public class Solution {
             return 0;
         int[] profit = new int[prices.length];
         int minPrice = prices[0];
-        profit[0] = prices[0];
+        profit[0] = 0;
         for(int i = 1; i < prices.length; i++) {
             minPrice = Math.min(minPrice, prices[i]);
             profit[i] = Math.max(profit[i-1], prices[i]-minPrice);
