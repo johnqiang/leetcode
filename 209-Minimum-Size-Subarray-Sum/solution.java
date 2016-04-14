e959391f17ab40a3b79b8e2013dc20ef8856bfd0
QIANG ZHANG <zqfly2013@gmail.com>
2016-04-14 12:09:58 -0400

    [209. Minimum Size Subarray Sum][Wrong Answer]committed by johnson

--- 209-Minimum-Size-Subarray-Sum/solution.java
+++ 209-Minimum-Size-Subarray-Sum/solution.java
@@ -1,6 +1,8 @@
 public class Solution {
     public int minSubArrayLen(int s, int[] nums) {
         int n = nums.length, min = n, left = 0, right = 0, sum = 0;
+        if (nums.length == 0)
+            return 0;
 		while(right < n){
 			while(right < n && sum < s){
 				sum += nums[right++];
