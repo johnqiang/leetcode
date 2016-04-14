f5beeb45dd88b4626d0ec883ef76de969bd06249
QIANG ZHANG <zqfly2013@gmail.com>
2016-04-14 00:25:46 -0400

    [209. Minimum Size Subarray Sum][Time Limit Exceeded]committed by johnson

---
+++ 209-Minimum-Size-Subarray-Sum/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        int minLength = nums.length;
+        for(int i = 0; i < nums.length; i++) {
+            int sum = nums[i];
+            for(int j = i+1; j < nums.length; j++){
+                sum += nums[j];
+                if(sum >= s && (j-i+1) < minLength)
+                    minLength = j-i+1; 
+            }
+        }
+        return minLength;
+    }
+}
