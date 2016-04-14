7b07f673b4565a8a6c7affc1086902ffb6a785e0
QIANG ZHANG <zqfly2013@gmail.com>
2016-04-13 23:50:15 -0400

    [169. Majority Element][Accepted]committed by johnson

--- 169-Majority-Element/solution.java
+++ 169-Majority-Element/solution.java
@@ -1,16 +1,18 @@
 public class Solution {
     public int majorityElement(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        map.put(nums[0], 1);
-        for (int i = 1; i < nums.length; i++) {
-            if (map.get(nums[i]) > nums.length / 2)
-                return nums[i];
+        for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]++));
+                map.put(nums[i], map.get(nums[i])+1);
             }else{
                 map.put(nums[i], 1);
             }
         }
-        return 0;
+        for (int item: map.keySet()){
+        	if (map.get(item) > nums.length / 2){
+        		return item;
+        	}
+        }
+        return -1;
     }
 }
